31d539039872ddb0906997b9d6af2da3a21c51e2
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-12-04 16:27:18 +0100

    [IMP] make purchase price computation overloadable

--- account_invoice_margin/models/account_invoice.py
+++ account_invoice_margin/models/account_invoice.py
@@ -31,6 +31,12 @@ class AccountInvoiceLine(models.Model):
         string='Cost',
     )
 
+    def _get_purchase_price(self):
+        # Overwrite this function if you don't want to base your
+        # purchase price on the product standard_price
+        self.ensure_one()
+        return self.product_id.standard_price
+
     @api.depends('purchase_price', 'price_subtotal')
     def _compute_margin(self):
         applicable = self.filtered(
@@ -51,7 +57,7 @@ class AccountInvoiceLine(models.Model):
     @api.onchange('product_id', 'uom_id')
     def _onchange_product_id_account_invoice_margin(self):
         if self.invoice_id.type in ['out_invoice', 'out_refund']:
-            purchase_price = self.product_id.standard_price
+            purchase_price = self._get_purchase_price()
             if self.uom_id != self.product_id.uom_id:
                 purchase_price = self.product_id.uom_id._compute_price(
                     purchase_price, self.uom_id)
